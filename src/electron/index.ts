18fd79414e5103f73cd185d630a69fc08bc4f1f9
Matthew Hemingway <dpplrdr435@gmail.com>
2020-07-15 13:03:43 -0400

    Create site skeleteon

--- src/electron/index.ts
+++ src/electron/index.ts
@@ -1,7 +1,8 @@
 import { app, BrowserWindow } from 'electron';
+import path from 'path';
 
 app.on('ready', () => {
   const window = new BrowserWindow({ width: 800, height: 800 });
   window.setMenuBarVisibility(false);
-  window.loadURL('http://example.com');
+  window.loadFile(path.resolve(__dirname, '../website/index.html'));
 });
---
+++ src/website/index.ts
@@ -0,0 +1,2 @@
+const app = document.getElementById('app');
+if (app) app.innerHTML = 'Hello world!';
